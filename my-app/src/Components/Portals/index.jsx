330cb9b236b378e1a23234def29144b78c17185a
Betty985 <1830265753@qq.com>
2022-07-19 18:48:40 +0800

    feat : portal 事件冒泡

--- my-app/src/Components/Portals/index.jsx
+++ my-app/src/Components/Portals/index.jsx
@@ -11,23 +11,35 @@ import {useEffect, useState} from 'react'
 }
  function ModalControl(props){
    let [state,setState]=useState(false)
+   let [count,setCount]=useState(0)
    let handleShow=()=>{
     setState(true)
 }
    let handleHide=()=>{setState(false)}
+   let handleClick=()=>{
+    if(state){
+        handleHide()      
+    }else{
+        handleShow()
+        setCount(++count)
+    }
+   }
    const modal=state?(
     <Modal container={props.container}>
         <div className="modal">
             <div>
                 对话框
             </div>
-            <button onClick={handleHide}>关闭</button>
+            <button>关闭</button>
         </div>
     </Modal>
    ):null
    return (
-    <div>
-        <button onClick={handleShow}>打开</button>
+    <div onClick={handleClick}>
+        <p>对话框打开了{count}次！</p>
+        {/*   这个按钮的点击事件会冒泡到父元素
+  因为这里没有定义 'onClick' 属性 */}
+        <button >打开</button>
         {modal}
     </div>
    )
