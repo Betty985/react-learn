aca7aaee95f2f9b04f3761fd08b896b13d98eb7d
Space <1830265753@qq.com>
2022-07-22 12:01:07 +0800

    fix:不能显示

--- my-app/src/hooks/useMouse.jsx
+++ my-app/src/hooks/useMouse.jsx
@@ -1,11 +1,13 @@
+import React from "react"
 const { useState } = require("react")
 export function useMouse(){
     let [x, setX] = useState(0)
     let [y, setY] = useState(0)
+    const MouseContext = React.createContext({ x: 0, y: 0 })
     return {
-        x,
-        y,
-        handleMouseMove:function handleMouseMove(event) {
+       mouse:{x,y},
+       MouseContext,
+       handleMouseMove:function handleMouseMove(event) {
             setX(event.clientX)
             setY(event.clientY)
         }
