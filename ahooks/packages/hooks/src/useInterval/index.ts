562d903a3cffd4570a7d1ddf0c3fbf024912a198
Betty985 <1830265753@qq.com>
2022-07-25 15:05:21 +0800

    最后一定要加上  / ，表示将这个文件夹加入，而不是将这个文件夹当做一个子模块。

---
+++ ahooks/packages/hooks/src/useInterval/index.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef } from 'react';
+import useLatest from '../useLatest';
+import { isNumber } from '../utils';
+
+function useInterval(
+  fn: () => void,
+  delay: number | undefined,
+  options?: {
+    immediate?: boolean;
+  },
+) {
+  const immediate = options?.immediate;
+
+  const fnRef = useLatest(fn);
+  const timerRef = useRef<number | NodeJS.Timer>();
+
+  useEffect(() => {
+    if (!isNumber(delay) || delay < 0) return;
+
+    if (immediate) {
+      fnRef.current();
+    }
+    timerRef.current = setInterval(() => {
+      fnRef.current();
+    }, delay);
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current as NodeJS.Timer);
+      }
+    };
+  }, [delay]);
+
+  const clear = useCallback(() => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current as NodeJS.Timer);
+    }
+  }, []);
+
+  return clear;
+}
+
+export default useInterval;
