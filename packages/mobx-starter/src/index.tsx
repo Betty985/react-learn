5eb8c87744914743405ecd6212f7cfacd6393261
Betty985 <1830265753@qq.com>
2022-08-02 17:37:06 +0800

    mobx：样式

--- packages/mobx-starter/src/index.tsx
+++ packages/mobx-starter/src/index.tsx
@@ -1,6 +1,25 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import { Timer } from "./components/index";
-import { FC } from "react";
-const App: FC = () =>  (<Timer />) ;
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Timer, Todo } from "./components/index";
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="Timer" element={<Timer />} />
+        <Route path="Todo" element={<Todo />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
