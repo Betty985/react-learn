4cf64c1715d112dce45b699027a5fc72cfbaf946
Betty985 <1830265753@qq.com>
2022-08-04 14:45:10 +0800

    mini-ahooks：useTimeout

--- packages/project-starter/src/containers/Container.tsx
+++ packages/project-starter/src/containers/Container.tsx
@@ -1,2 +1,7 @@
-let a=()=>(<h1>Container</h1>)
-export default a
+import React, { FC } from "react";
+import Header from '../layout/Header';
+import { Route, Switch } from 'react-router-dom';
+let a: FC = () => {
+  return <h1>Container</h1>;
+};
+export default a;
