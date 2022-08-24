822b71f5c7606c40c0195855398ca21bfc965761
Betty985 <1830265753@qq.com>
2022-08-24 16:18:27 +0800

    perf(custom): (conduit):热门标签显示

--- packages/admin-starter/src/pages/Home/components/Tags.tsx
+++ packages/admin-starter/src/pages/Home/components/Tags.tsx
@@ -1,22 +1,25 @@
-import React, { FC, memo, useEffect } from "react";
+import React, { FC, memo, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../../components/LoadingSpinner"
 import useStores from "../../../hooks/useStores";
 
 const Tags: FC = () => {
     const { commonStore } = useStores();
+    const [isLoadingTags,setIsLoadingTags]=useState(true)
+    const [tags,setTags]=useState([])
     useEffect(() => {
-        commonStore.loadTags();
+        commonStore.loadTags().then(()=>{
+            setTags(commonStore.tags)
+            setIsLoadingTags(commonStore.isLoadingTags)
+        })
     }, [])
-    const { tags, isLoadingTags } = commonStore
 
-    console.log(tags)
     return (
         <div className="col-md-3">
             <div className="sidebar">
                 <p>Popular Tags</p>
                 <div className="tag-list">
-                    {isLoadingTags && tags.length == 0 ? <LoadingSpinner />
+                    {isLoadingTags ? <LoadingSpinner />
                         : tags.map(tag =>
                             <Link
                                 to={{
